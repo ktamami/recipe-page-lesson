947c5466d461414177a048b6834ddda79f190773
ktamami <kandotamami@gmail.com>
2022-05-06 13:52:10 +0900

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,10 @@ const controlAddRecipe = async function (newRecipe) {
     console.error("👻👻", err);
     addRecipeView.renderError(err.message);
   }
+
+  const newFeature = function () {
+    console.log("Hello");
+  };
 };
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
